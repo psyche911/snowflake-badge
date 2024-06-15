d2648c8133e4557560d4970d4c57dc71daebdec8
DMAO-MQ <33108357+psyche911@users.noreply.github.com>
2024-06-15 21:33:40 +1000

    Update masterclass_complete_2023.sql

--- masterclass_complete_2023.sql
+++ masterclass_complete_2023.sql
@@ -856,3 +856,368 @@ COPY INTO EXERCISE_DB.PUBLIC.EMPLOYEES
     --validation_mode = return_errors
     truncatecolumns = true
     ;
+
+
+------- Load Semi-Structured Data ---------
+
+// First Step: Load Raw JSON
+
+CREATE OR REPLACE stage MANAGE_DB.EXTERNAL_STAGES.JSONSTAGE
+     url = 's3://bucketsnowflake-jsondemo';
+
+list @MANAGE_DB.EXTERNAL_STAGES.JSONSTAGE;
+
+CREATE OR REPLACE file format MANAGE_DB.FILE_FORMATS.JSONFORMAT
+    TYPE = JSON;    
+    
+CREATE OR REPLACE table OUR_FIRST_DB.PUBLIC.JSON_RAW (
+    raw_file variant);
+    
+COPY INTO OUR_FIRST_DB.PUBLIC.JSON_RAW
+    FROM @MANAGE_DB.EXTERNAL_STAGES.JSONSTAGE
+    file_format = MANAGE_DB.FILE_FORMATS.JSONFORMAT
+    files = ('HR_data.json');    
+   
+SELECT * FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+---- Assignment 7 ----
+
+create or replace stage exercise_db.external_stages.aws_stage_json
+    url = 's3://snowflake-assignments-mc/unstructureddata/';
+
+list @exercise_db.external_stages.aws_stage_json;
+
+create or replace file format exercise_db.public.json_file_format
+    type = JSON;
+
+create or replace table exercise_db.public.json_raw (
+    raw_file variant);
+
+copy into exercise_db.public.json_raw
+    from @exercise_db.external_stages.aws_stage_json
+    file_format = exercise_db.public.json_file_format
+    files = ('Jobskills.json');
+
+select * from exercise_db.public.json_raw
+limit 1;
+
+
+// Second Step: Parse & Analyse Raw JSON 
+
+// Selecting attribute/column
+
+SELECT RAW_FILE:city FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+SELECT $1:first_name FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+// Selecting attribute/column - formattted
+
+SELECT RAW_FILE:first_name::string as first_name  FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+SELECT RAW_FILE:id::int as id  FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+SELECT 
+    RAW_FILE:id::int as id,  
+    RAW_FILE:first_name::STRING as first_name,
+    RAW_FILE:last_name::STRING as last_name,
+    RAW_FILE:gender::STRING as gender
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+
+// Handling Nested Data
+   
+SELECT RAW_FILE:job as job  FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+
+SELECT 
+    RAW_FILE:job.salary::INT as salary
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+
+SELECT 
+    RAW_FILE:first_name::STRING as first_name,
+    RAW_FILE:job.salary::INT as salary,
+    RAW_FILE:job.title::STRING as title
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+// Handling Arreys
+
+SELECT
+    RAW_FILE:prev_company as prev_company
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+
+SELECT
+    RAW_FILE:prev_company[1]::STRING as prev_company
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+
+SELECT
+    ARRAY_SIZE(RAW_FILE:prev_company) as prev_company
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+
+SELECT 
+    RAW_FILE:id::int as id,  
+    RAW_FILE:first_name::STRING as first_name,
+    RAW_FILE:prev_company[0]::STRING as prev_company
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW
+UNION ALL 
+SELECT 
+    RAW_FILE:id::int as id,  
+    RAW_FILE:first_name::STRING as first_name,
+    RAW_FILE:prev_company[1]::STRING as prev_company
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW
+ORDER BY id;
+
+
+---- Assignment 8 ----
+
+SELECT RAW_FILE:Skills FROM exercise_db.public.json_raw
+where RAW_FILE:first_name = 'Florina';
+
+SELECT 
+    $1:first_name::string as first_name,
+    $1:last_name::string as last_name,
+    $1:Skills[0]::string as skills_1,
+    $1:Skills[1]::string as skills_2
+FROM exercise_db.public.json_raw;
+
+create or replace table exercise_db.public.skills as
+SELECT 
+    $1:first_name::string as first_name,
+    $1:last_name::string as last_name,
+    $1:Skills[0]::string as skills_1,
+    $1:Skills[1]::string as skills_2
+FROM exercise_db.public.json_raw;
+
+select * from exercise_db.public.skills
+where first_name = 'Florina';
+
+
+---- Deal with Hierarchy ----
+
+SELECT 
+    RAW_FILE:spoken_languages as spoken_languages
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+SELECT * FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+
+SELECT 
+     array_size(RAW_FILE:spoken_languages) as spoken_languages
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+
+SELECT 
+     RAW_FILE:first_name::STRING as first_name,
+     array_size(RAW_FILE:spoken_languages) as spoken_languages
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+
+SELECT 
+    RAW_FILE:spoken_languages[0] as First_language
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+
+SELECT 
+    RAW_FILE:first_name::STRING as first_name,
+    RAW_FILE:spoken_languages[0] as First_language
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+
+SELECT 
+    RAW_FILE:first_name::STRING as First_name,
+    RAW_FILE:spoken_languages[0].language::STRING as First_language,
+    RAW_FILE:spoken_languages[0].level::STRING as Level_spoken
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+
+SELECT 
+    RAW_FILE:id::int as id,
+    RAW_FILE:first_name::STRING as First_name,
+    RAW_FILE:spoken_languages[0].language::STRING as First_language,
+    RAW_FILE:spoken_languages[0].level::STRING as Level_spoken
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW
+UNION ALL 
+SELECT 
+    RAW_FILE:id::int as id,
+    RAW_FILE:first_name::STRING as First_name,
+    RAW_FILE:spoken_languages[1].language::STRING as First_language,
+    RAW_FILE:spoken_languages[1].level::STRING as Level_spoken
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW
+UNION ALL 
+SELECT 
+    RAW_FILE:id::int as id,
+    RAW_FILE:first_name::STRING as First_name,
+    RAW_FILE:spoken_languages[2].language::STRING as First_language,
+    RAW_FILE:spoken_languages[2].level::STRING as Level_spoken
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW
+ORDER BY ID;
+
+
+select
+    RAW_FILE:first_name::STRING as First_name,
+    f.value:language::STRING as First_language,
+    f.value:level::STRING as Level_spoken
+from OUR_FIRST_DB.PUBLIC.JSON_RAW, table(flatten(RAW_FILE:spoken_languages)) f;
+
+
+---- Insert Final Table ----
+
+// Option 1: CREATE TABLE AS
+
+CREATE OR REPLACE TABLE Languages AS
+select
+    RAW_FILE:first_name::STRING as First_name,
+    f.value:language::STRING as First_language,
+    f.value:level::STRING as Level_spoken
+from OUR_FIRST_DB.PUBLIC.JSON_RAW, table(flatten(RAW_FILE:spoken_languages)) f;
+
+SELECT * FROM Languages;
+
+truncate table languages;
+
+// Option 2: INSERT INTO
+
+INSERT INTO Languages
+select
+    RAW_FILE:first_name::STRING as First_name,
+    f.value:language::STRING as First_language,
+    f.value:level::STRING as Level_spoken
+from OUR_FIRST_DB.PUBLIC.JSON_RAW, table(flatten(RAW_FILE:spoken_languages)) f;
+
+SELECT * FROM Languages;
+
+
+---- PARQUET Data ----
+
+// Create file format and stage object
+    
+CREATE OR REPLACE FILE FORMAT MANAGE_DB.FILE_FORMATS.PARQUET_FORMAT
+    TYPE = 'parquet';
+
+CREATE OR REPLACE STAGE MANAGE_DB.EXTERNAL_STAGES.PARQUETSTAGE
+    url = 's3://snowflakeparquetdemo'   
+    FILE_FORMAT = MANAGE_DB.FILE_FORMATS.PARQUET_FORMAT;
+    
+// Preview the data
+    
+LIST  @MANAGE_DB.EXTERNAL_STAGES.PARQUETSTAGE;
+    
+SELECT * FROM @MANAGE_DB.EXTERNAL_STAGES.PARQUETSTAGE;
+
+// File format in Queries
+
+CREATE OR REPLACE STAGE MANAGE_DB.EXTERNAL_STAGES.PARQUETSTAGE
+    url = 's3://snowflakeparquetdemo';
+    
+SELECT * 
+FROM @MANAGE_DB.EXTERNAL_STAGES.PARQUETSTAGE
+(file_format => 'MANAGE_DB.FILE_FORMATS.PARQUET_FORMAT');
+
+// Quotes can be omitted in case of the current namespace
+USE MANAGE_DB.FILE_FORMATS;
+
+SELECT * 
+FROM @MANAGE_DB.EXTERNAL_STAGES.PARQUETSTAGE
+(file_format => MANAGE_DB.FILE_FORMATS.PARQUET_FORMAT);
+
+CREATE OR REPLACE STAGE MANAGE_DB.EXTERNAL_STAGES.PARQUETSTAGE
+    url = 's3://snowflakeparquetdemo'
+    FILE_FORMAT = MANAGE_DB.FILE_FORMATS.PARQUET_FORMAT;
+
+// Syntax for Querying unstructured data
+
+SELECT 
+$1:__index_level_0__,
+$1:cat_id,
+$1:date,
+$1:"__index_level_0__",
+$1:"cat_id",
+$1:"d",
+$1:"date",
+$1:"dept_id",
+$1:"id",
+$1:"item_id",
+$1:"state_id",
+$1:"store_id",
+$1:"value"
+FROM @MANAGE_DB.EXTERNAL_STAGES.PARQUETSTAGE;
+
+// Date conversion
+    
+SELECT 1;
+
+SELECT DATE(365*60*60*24);
+
+// Querying with conversions and aliases
+    
+SELECT 
+$1:__index_level_0__::int as index_level,
+$1:cat_id::VARCHAR(50) as category,
+DATE($1:date::int ) as Date,
+$1:"dept_id"::VARCHAR(50) as Dept_ID,
+$1:"id"::VARCHAR(50) as ID,
+$1:"item_id"::VARCHAR(50) as Item_ID,
+$1:"state_id"::VARCHAR(50) as State_ID,
+$1:"store_id"::VARCHAR(50) as Store_ID,
+$1:"value"::int as value
+FROM @MANAGE_DB.EXTERNAL_STAGES.PARQUETSTAGE;
+
+
+---- Adding Metadata ----
+    
+SELECT 
+    $1:__index_level_0__::int as index_level,
+    $1:cat_id::VARCHAR(50) as category,
+    DATE($1:date::int ) as Date,
+    $1:"dept_id"::VARCHAR(50) as Dept_ID,
+    $1:"id"::VARCHAR(50) as ID,
+    $1:"item_id"::VARCHAR(50) as Item_ID,
+    $1:"state_id"::VARCHAR(50) as State_ID,
+    $1:"store_id"::VARCHAR(50) as Store_ID,
+    $1:"value"::int as value,
+    METADATA$FILENAME as FILENAME,
+    METADATA$FILE_ROW_NUMBER as ROWNUMBER,
+    TO_TIMESTAMP_NTZ(current_timestamp) as LOAD_DATE
+FROM @MANAGE_DB.EXTERNAL_STAGES.PARQUETSTAGE;
+
+SELECT TO_TIMESTAMP_NTZ(current_timestamp);
+
+// Create destination table
+
+CREATE OR REPLACE TABLE OUR_FIRST_DB.PUBLIC.PARQUET_DATA (
+    ROW_NUMBER int,
+    index_level int,
+    cat_id VARCHAR(50),
+    date date,
+    dept_id VARCHAR(50),
+    id VARCHAR(50),
+    item_id VARCHAR(50),
+    state_id VARCHAR(50),
+    store_id VARCHAR(50),
+    value int,
+    Load_date timestamp default TO_TIMESTAMP_NTZ(current_timestamp));
+
+// Load the parquet data
+   
+COPY INTO OUR_FIRST_DB.PUBLIC.PARQUET_DATA
+    FROM (SELECT 
+            METADATA$FILE_ROW_NUMBER,
+            $1:__index_level_0__::int,
+            $1:cat_id::VARCHAR(50),
+            DATE($1:date::int ),
+            $1:"dept_id"::VARCHAR(50),
+            $1:"id"::VARCHAR(50),
+            $1:"item_id"::VARCHAR(50),
+            $1:"state_id"::VARCHAR(50),
+            $1:"store_id"::VARCHAR(50),
+            $1:"value"::int,
+            TO_TIMESTAMP_NTZ(current_timestamp)
+        FROM @MANAGE_DB.EXTERNAL_STAGES.PARQUETSTAGE);       
+    
+SELECT * FROM OUR_FIRST_DB.PUBLIC.PARQUET_DATA;
+
+
+
